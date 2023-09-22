c2c3bb75309127538363ba5bbf1a3d65c498ba0c
Ogogost <Wizard132>
2023-09-22 15:18:15 +0300

    test

--- ocr.py
+++ ocr.py
@@ -1,8 +1,10 @@
+
+
 # import the necessary packages
 import pytesseract
 from PIL import Image
 
-image = Image.open("ss1.png")
+image = Image.open("ss2.png")
 string = pytesseract.image_to_string(image, lang='rus')
 
 print(string)
